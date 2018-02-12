8af588dd37c0aec50c0dee9cbb021e3dc7989228
Prian Kuhanandan <priankuhan@gmail.com>
2018-02-12 10:46:53 +0800

    Echo the deleted records to the user on `clear`
    Displays all the records that have been deleted due to the `clear`
    action after executing the `clear`. This is useful for users who have
    accidently cleared their data and want to recover it.
    User Guide and I/O tests were modified appropriately according to the
    changes

--- doc/UserGuide.md
+++ doc/UserGuide.md
@@ -112,7 +112,7 @@ Examples:
   Views all details of the 1st person in the results of the `find` command.
 
 ### Clearing all entries : `clear`
-Clears all entries from the address book.<br>
+Clears all entries from the address book. Also lists all deleted entries after deletion so that the information may be recovered if necessary<br>
 Format: `clear`  
 
 #### Exiting the program : `exit`
--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import java.util.List;
+
 /**
  * Clears the address book.
  */
@@ -9,14 +12,16 @@ public class ClearCommand extends Command {
     public static final String MESSAGE_USAGE = "Clears address book permanently.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Address book has been cleared!\n"
+            + "The persons listed above have been deleted.";
 
     public ClearCommand() {}
 
 
     @Override
     public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
         addressBook.clear();
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_SUCCESS, allPersons);
     }
 }
